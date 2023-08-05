3e42b175064cd9ca3f5ea087122c5003ce9ab945
imbenayad <Imad.BENAYAD@etu.uca.fr>
2023-08-05 19:29:49 +0200

    Syslog test

---
+++ chapter1/syslog/test_syslog.c
@@ -0,0 +1,27 @@
+#include <syslog.h>
+#include <sys/time.h>
+
+// Note that all syslog output goes to /var/log/syslog
+//
+// To view the output as your RT applicaiton is running, you can just tail the log as follows:
+//
+// tail -f /var/log/syslog
+//
+// Syslog is more efficient than printf and causes less potential for "blocking" in the middle of your
+// service execution.  Please use it instead of printf and use it because it is simpler and easier to use
+// than ftrace (although ftrace is ideal, it is harder to learn and master).
+//
+// https://elinux.org/Ftrace
+//
+// So, syslog is a nice compromise between efficent software-in-circuit tracing and printf.
+
+
+void main(void)
+{
+    struct timeval tv;
+
+    gettimeofday(&tv, (struct timezone *)0);
+    syslog(LOG_CRIT, "My log message test @ tv.tv_sec %d, tv.tv_usec %d\n", tv.tv_sec, tv.tv_usec);
+    gettimeofday(&tv, (struct timezone *)0);
+    syslog(LOG_CRIT, "My log message test @ tv.tv_sec %d, tv.tv_usec %d\n", tv.tv_sec, tv.tv_usec);
+}
